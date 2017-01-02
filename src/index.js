b75e113d7a847c7e35d936eecb254fbc1732b56e
William Belle <william.belle@gmail.com>
2017-01-02 18:22:04 +0100

    implement HONcode certification list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 'use strict';
+
+var os       = require('os');
+var fs       = require('fs');
+var download = require('download');
+var Promise  = require('bluebird');
+
+var url = 'https://www.hon.ch/HONcode/Plugin/listeMD5.txt';
+var fileName = 'HONcodeMD5List.txt';
+
+var buildHash = function(data) {
+  var map = {};
+
+  if (data !== null && data !== '') {
+    var lines = data.split(new RegExp('\n', 'i'));
+    for (var i = 0; i < lines.length - 1; i++) {
+      var infos = lines[i].split(new RegExp(' ', 'i'));
+      map[infos[0]] = infos[1];
+    }
+  }
+  return map;
+};
+
+var isOutdated = function(file) {
+  var stats = fs.statSync(file);
+  var lastModified = new Date(stats.mtime);
+  var now = new Date();
+  return now.getTime() - lastModified.getTime() > 3600 * 1000;
+};
+
+var retrieveMd5List = function() {
+  var promise = new Promise(function(resolve) {
+    var tmpDir = os.tmpDir();
+    var fileExist = fs.existsSync(tmpDir + '/' + fileName);
+    var isTooOld = false;
+    if (fileExist) {
+      isTooOld = isOutdated(tmpDir + '/' + fileName);
+    }
+    if (fileExist && !isTooOld) {
+      var content = fs.readFileSync(tmpDir + '/' + fileName, 'utf8');
+      var hash = buildHash(content);
+      resolve(hash);
+    } else {
+      download(url).then(function(data) {
+        fs.writeFileSync(tmpDir + '/' + fileName, data);
+        var hash = buildHash(data.toString());
+        resolve(hash);
+      }).catch(function(reason) {
+        console.log(reason);
+      });
+    }
+  });
+  return promise;
+};
+
+exports.getHONcodeCertificationList = function() {
+  return retrieveMd5List();
+};
--- test/test.js
+++ test/test.js
@@ -1,1 +1,37 @@
 'use strict';
+
+var should = require('chai').should();
+var honcodeCertificationList = require('../src/index.js');
+
+describe('HONcode Certification List', function() {
+
+  var md5List;
+
+  beforeEach(function(done) {
+    honcodeCertificationList.getHONcodeCertificationList().then(function(hash) {
+      md5List = hash;
+      done();
+    });
+  });
+
+  it('should contain at least 5000 items', function(done) {
+    var enoughListLength = Object.keys(md5List).length > 5000;
+    enoughListLength.should.equal(true);
+    done();
+  });
+
+  it('should contain HONConduct298987', function(done) {
+    md5List[
+      '995da273245834a9ab8bdfe4d7db0363'
+    ].should.equal('HONConduct298987');
+    done();
+  });
+
+  it('should not contain playboy.com', function(done) {
+    should.equal(md5List[
+      '91bad729a69dfbef464400f92cf4401a' // Do md5('playboy.com/')
+    ], undefined);
+    done();
+  });
+
+});

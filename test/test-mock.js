13eebc11968550c2f38b419ed8dad2c095979d01
William Belle <william.belle@gmail.com>
2017-01-18 21:35:10 +0100

    test Promise catch

---
+++ test/test-mock.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var os = require('os');
+var fs = require('fs');
+
+var should = require('chai').should();
+var rewire = require('rewire');
+
+var honcodeCertificationList = rewire('../src/index.js');
+
+describe('HONcode Certification Foobar List', function() {
+
+  before(function(done) {
+    var tmpDir = os.tmpDir();
+    if (fs.existsSync(tmpDir + '/HONcodeMD5List.txt')) {
+      fs.unlinkSync(tmpDir + '/HONcodeMD5List.txt');
+    }
+    done();
+  });
+
+  it('should fail with foobar list', function(done) {
+    honcodeCertificationList.__set__('url', 'foobar');
+    var result = honcodeCertificationList.getHONcodeCertificationList();
+    result.then(function(hash) {
+      should.fail();
+      done();
+    }).catch(function(reason) {
+      done();
+    });
+  });
+});
